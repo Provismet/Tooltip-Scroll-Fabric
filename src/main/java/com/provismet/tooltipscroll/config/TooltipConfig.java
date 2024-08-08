da4593885807125680d1a3876b4985a0f3e1e546
Provismet <17149901+Provismet@users.noreply.github.com>
2024-08-09 00:12:24 +0100

    Revert distance to speed.
    Distance is the correct terminology from a programmatic perspective. But to an end user, speed is the correct term due to it being more intuitive and related to the act of scrolling.

--- src/main/java/com/provismet/tooltipscroll/config/TooltipConfig.java
+++ src/main/java/com/provismet/tooltipscroll/config/TooltipConfig.java
@@ -55,15 +55,15 @@ public class TooltipConfig {
                 .setSaveConsumer(newValue -> Options.invertYScroll = newValue)
                 .build());
 
-        general.addEntry(entryBuilder.startIntField(Text.translatable("entry.tooltipscroll.scrolldistance"), ScrollTracker.scrollSize)
+        general.addEntry(entryBuilder.startIntField(Text.translatable("entry.tooltipscroll.scrollspeed"), ScrollTracker.scrollSize)
             .setDefaultValue(10)
-            .setTooltip(Text.translatable("entrytooltip.tooltipscroll.scrolldistance"))
+            .setTooltip(Text.translatable("entrytooltip.tooltipscroll.scrollspeed"))
             .setSaveConsumer(newValue -> ScrollTracker.scrollSize = (int)MathHelper.absMax(1, newValue))
             .build());
         
-        general.addEntry(entryBuilder.startIntField(Text.translatable("entry.tooltipscroll.scrolldistancekeys"), ScrollTracker.scrollSizeKeyboard)
+        general.addEntry(entryBuilder.startIntField(Text.translatable("entry.tooltipscroll.scrollspeedkeys"), ScrollTracker.scrollSizeKeyboard)
             .setDefaultValue(5)
-            .setTooltip(Text.translatable("entrytooltip.tooltipscroll.scrolldistancekeys"))
+            .setTooltip(Text.translatable("entrytooltip.tooltipscroll.scrollspeedkeys"))
             .setSaveConsumer(newValue -> ScrollTracker.scrollSizeKeyboard = (int)MathHelper.absMax(1, newValue))
             .build());
 
@@ -76,10 +76,7 @@ public class TooltipConfig {
             })
             .build());
 
-        builder.setSavingRunnable(() ->{
-            Options.saveJSON();
-        });
-
+        builder.setSavingRunnable(Options::saveJSON);
         return builder.build();
     }
 }
