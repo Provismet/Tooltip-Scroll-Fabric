111e042232f4176ab38bc622ff7af626fac063e4
unknown-salt <217187386+unknown-salt@users.noreply.github.com>
2025-11-07 13:34:40 +0000

    Add ability to start on top

--- src/main/java/com/provismet/tooltipscroll/mixin/DrawContextMixin.java
+++ src/main/java/com/provismet/tooltipscroll/mixin/DrawContextMixin.java
@@ -1,19 +1,20 @@
 package com.provismet.tooltipscroll.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.ref.LocalIntRef;
+import com.provismet.tooltipscroll.ScrollTracker;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.tooltip.TooltipComponent;
 import net.minecraft.client.gui.tooltip.TooltipPositioner;
-import java.util.List;
-
-import com.provismet.tooltipscroll.ScrollTracker;
 import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
 
 @Mixin(DrawContext.class)
 public abstract class DrawContextMixin {
@@ -29,26 +30,20 @@ public abstract class DrawContextMixin {
 		ScrollTracker.setItem(components);
 	}
 
-	// Using an invoke inject here because the tooltip coordinates get checked for out of bounds positions. I want the scroll offset to only apply after the bound check.
-	// Targeting a method invoke because it was just conveniently placed after the bound check.
-
-	// l is the variable that determines y-axis position of a tooltip.
-	@ModifyVariable(
-		method = "drawTooltip(Lnet/minecraft/client/font/TextRenderer;Ljava/util/List;IILnet/minecraft/client/gui/tooltip/TooltipPositioner;Lnet/minecraft/util/Identifier;)V",
-		ordinal = 7,
-		at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/math/MatrixStack;push()V", shift = At.Shift.BEFORE)
+	@Inject(
+        method = "drawTooltip(Lnet/minecraft/client/font/TextRenderer;Ljava/util/List;IILnet/minecraft/client/gui/tooltip/TooltipPositioner;Lnet/minecraft/util/Identifier;)V",
+        at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/math/MatrixStack;push()V", shift = At.Shift.BEFORE)
 	)
-	private int modifyYAxis (int y) {
-		return y + ScrollTracker.getYOffset();
-	}
+	private void editXY (TextRenderer textRenderer, List<TooltipComponent> components, int x, int y, TooltipPositioner positioner, Identifier texture, CallbackInfo info, @Local(ordinal = 6) LocalIntRef effectiveX, @Local(ordinal = 7) LocalIntRef effectiveY) {
+		effectiveX.set(effectiveX.get() + ScrollTracker.getXOffset());
+		effectiveY.set(effectiveY.get() + ScrollTracker.getYOffset());
 
-	// k is the variable that determines x-axis position of a tooltip.
-	@ModifyVariable(
-		method = "drawTooltip(Lnet/minecraft/client/font/TextRenderer;Ljava/util/List;IILnet/minecraft/client/gui/tooltip/TooltipPositioner;Lnet/minecraft/util/Identifier;)V",
-		ordinal = 6,
-		at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/math/MatrixStack;push()V", shift = At.Shift.BEFORE)
-	)
-	private int modifyXAxis (int x) {
-		return x + ScrollTracker.getXOffset();
+        if (!ScrollTracker.hasMoved()) {
+            int originalY = effectiveY.get();
+            if (effectiveY.get() < 4) {
+                effectiveY.set(4);
+                ScrollTracker.setInitialYOffset(4 - originalY);
+            }
+        }
 	}
 }
