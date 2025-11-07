ba8a5e5ca87f6c2c008993634cd3192f8b4dd968
unknown-salt <217187386+unknown-salt@users.noreply.github.com>
2025-11-07 14:34:40 +0100

    Add ability to start on top

--- src/main/java/com/provismet/tooltipscroll/mixin/DrawContextMixin.java
+++ src/main/java/com/provismet/tooltipscroll/mixin/DrawContextMixin.java
@@ -30,9 +30,20 @@ public abstract class DrawContextMixin {
 		ScrollTracker.setItem(components);
 	}
 
-	@Inject(method = "drawTooltipImmediately", at = @At(value = "INVOKE", target = "Lorg/joml/Matrix3x2fStack;pushMatrix()Lorg/joml/Matrix3x2fStack;"))
+	@Inject(
+			method = "drawTooltipImmediately",
+			at = @At(value = "INVOKE", target = "Lorg/joml/Matrix3x2fStack;pushMatrix()Lorg/joml/Matrix3x2fStack;")
+	)
 	private void editXY (TextRenderer textRenderer, List<TooltipComponent> components, int x, int y, TooltipPositioner positioner, @Nullable Identifier texture, CallbackInfo info, @Local(ordinal = 6) LocalIntRef effectiveX, @Local(ordinal = 7) LocalIntRef effectiveY) {
 		effectiveX.set(effectiveX.get() + ScrollTracker.getXOffset());
 		effectiveY.set(effectiveY.get() + ScrollTracker.getYOffset());
+
+    if (!ScrollTracker.hasMoved()) {
+			int originalY = effectiveY.get();
+			if (effectiveY.get() < 4) {
+				effectiveY.set(4);
+				ScrollTracker.setInitialYOffset(4 - originalY);
+			}
+    }
 	}
 }
